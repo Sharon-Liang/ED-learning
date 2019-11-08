b947c65145af08c96feeaca778f259f6a4710aa1
Sharon-Liang <liangshuang228@gmail.com>
2019-11-08 13:59:35 -0800

    initiate

---
+++ 1D_Bose_Hubbard_Hardcore.py
@@ -0,0 +1,36 @@
+# This is a practice of using ED to solve 1D Bose-Hubbard Model
+# We use hardcore boson here
+import numpy as np
+import scipy as sp
+from scipy.sparse.linalg import eigsh
+import matplotlib.pyplot as plt
+
+'''Tag functions'''    
+def is_prime(num):
+    factor = 2
+    while(factor * factor <= num):
+        if num % factor == 0:
+            return False
+        factor += 1
+    return True
+
+def primes(M):
+    '''return a list of the first M prime numbers'''
+    count = 0
+    num = 2
+    primes = np.zeros(M)
+
+    while (count < M):
+        if is_prime(num):
+            primes[count] = num
+            count += 1
+        num += 1
+    return primes
+
+def find_tag(vec, M):
+    '''calculate the tag of each vectors'''
+    prime = primes(M)
+    tag = 0
+    for i in range(M):
+        tag += np.sqrt(prime[i]) * vec[i]
+    return tag
