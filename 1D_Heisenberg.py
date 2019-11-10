2f5ad8776825368cc1c6332db95d921ce045a3bd
Sharon-Liang <liangshuang228@gmail.com>
2019-11-10 10:11:54 -0800

    generate 2^N basis vectors for N-spin-1/2 Heisenberg chain.

--- 1D_Heisenberg.py
+++ 1D_Heisenberg.py
@@ -6,6 +6,8 @@ from scipy.sparse.linalg import eigsh
 import matplotlib.pyplot as plt
 
 '''Tag functions'''
+'''Note: We can use the corresponding decimal number of the basis states as
+         its tag.'''
 def is_prime(num):
     factor = 2
     while(factor * factor <= num):
@@ -55,19 +57,20 @@ def basis_vecs(N):
     dim = pow(2,N)
     basis = np.zeros([dim,N])
     for i in range(dim-1):
-        #determin the position of the first 0, start from the lowest
+        #determin the position of the first 0, start from the lowest digit
         for k in range(N-1,-1,-1):
             if basis[i,k] == 0:
                 break
         #write the next basis
         for j in range(k):
-            basis[i+1,j] == basis[i,j]
+            basis[i+1,j] = basis[i,j]
         basis[i+1,k] = 1
-        for j in range(k+1,N,1):
-            basis[i+1,k] = 0
+        if k + 1 < N:
+            for j in range(k+1,N):
+                basis[i+1,j] = 0
     return basis
 
-basis = basis_vecs(3)
+basis = basis_vecs(5)
 print(basis)
 
 def heisenberg(N):
