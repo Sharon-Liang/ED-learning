14efd112fb8c6eac22ce3da20c860bc505354b9c
Sharon-Liang <liangshuang228@gmail.com>
2019-11-14 13:29:31 -0800

    Add helper functions and modify Hamiltonian code

--- 1D_Heisenberg.py
+++ 1D_Heisenberg.py
@@ -2,40 +2,49 @@
 # Follow the instruction of arXiv: 1102.4006v1
 import numpy as np
 import scipy as sp
-from scipy.sparse.linalg import eigsh
+import scipy.stats as spst #Statistical functions
 import matplotlib.pyplot as plt
+
+from scipy.sparse.linalg import eigsh
 from scipy.special import comb, perm #Combination and Permulation operation
 
+#-------------------------------------------------------------------------------
+'''Helper functions: help with visualization and debugging'''
+def basisVisualizer(L,psi):
+    '''Given the dicimal tag of the state psi, outputs the state in arrows
+       L: total length of the chain'''
+    #ex: |↓|↑|↓|↑|↑|
+    psi_2 = bin(psi)[2:] # Alternative: psi_2 = format(psi,'b')
+    N  = len(psi_2)
+    up = (L-N)*'0'+psi_2 # add two string
+    configStr = "|"
+    uparrow   = '\u2191'
+    downarrow = '\u2193'
+    for i in range(L):
+        blank = True
+        if up[i] == '1':
+            configStr+=uparrow
+            blank = False
+        if up[i] == '0':
+            configStr+=downarrow
+            blank = False
+        if blank:
+            configStr+="_"
+        configStr +="|"
+    print(configStr)
+
+def countBits(x):
+    '''Counts number of 1s in bin(n)'''
+    #From Hacker's Delight, p. 66
+    x = x - ((x >> 1) & 0x55555555)
+    x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
+    x = (x + (x >> 4)) & 0x0F0F0F0F
+    x = x + (x >> 8)
+    x = x + (x >> 16)
+    return x & 0x0000003F
+
+#-------------------------------------------------------------------------------
 '''Tag functions'''
-def is_prime(num):
-    factor = 2
-    while(factor * factor <= num):
-        if num % factor == 0:
-            return False
-        factor += 1
-    return True
-
-def primes(M):
-    '''return a list of the first M prime numbers'''
-    count = 0
-    num = 2
-    primes = np.zeros(M)
-
-    while (count < M):
-        if is_prime(num):
-            primes[count] = num
-            count += 1
-        num += 1
-    return primes
-
-def find_tag(vec, M):
-    '''calculate the tag of each vectors'''
-    prime = primes(M)
-    tag = 0
-    for i in range(M):
-        tag += np.sqrt(prime[i]) * vec[i]
-    return tag
-
 '''Note: We can use the corresponding decimal number of the basis states as
          its tag.'''
 def decimal(vec):
@@ -46,6 +55,7 @@ def decimal(vec):
         dec += vec[i] * pow(2,N-1-i)
     return dec
 
+#-------------------------------------------------------------------------------
 '''Heisenberg model: H = J ∑_<ij> si ⋅ sj
    Number of sites: M
    Hilbert space dimension: 2^N
@@ -71,7 +81,6 @@ def basis_vecs(N):
                 basis[i+1,j] = 0
     return basis
 
-
 '''Rewrite Hamiltonian to two parts:
    Diagonal: Hz = Jz S^z S^z
    Off-diagonal: Hxy = Jxy/2 (S^+ S^-  +  S^- S^+)
@@ -79,32 +88,24 @@ def basis_vecs(N):
                  S- = S^x  - i S^y
    '''
 
-def heisenberg(N, Jz, Jxy, h, periodic = False, hermition = False):
+def heisenberg(N, Jz, Jxy, h, periodic = False):
     '''Build Heisenberg model Hamiltonian'''
-    dim = pow(2,N)
     basis = basis_vecs(N)
+    dim = len(basis)
     if periodic:
         limit = N
     else:
         limit = N-1
-    # Diagonal: Hz = Jz S^z S^z
-    Hz = np.zeros([dim,dim])
+
+    Hz = np.zeros([dim,dim]) # Diagonal: Hz = Jz S^z S^z
+    Hxy = np.zeros([dim,dim]) #Off-diagonal: Hxy = Jxy/2 (S^+ S^-  +  S^- S^+)
+    Hh = np.zeros([dim,dim]) # add small h to break degeneracy
+
     for i in range(dim):
         for j in range(limit):
-            if j == N-1:
-                next = 0
-            else:
-                next = j+1
+            next = (j+1) % N
             Hz[i,i] += - Jz * (basis[i,j] - 1/2) * (basis[i,next] - 1/2)
 
-    #Off-diagonal: Hxy = Jxy/2 (S^+ S^-  +  S^- S^+)
-    Hxy = np.zeros([dim,dim])
-    for i in range(dim):
-        for j in range(limit):
-            if j == N-1:
-                next = 0
-            else:
-                next = j + 1
             vec = np.zeros(N,int)
             vec[j] = 1
             vec[next] = 1
@@ -113,16 +114,13 @@ def heisenberg(N, Jz, Jxy, h, periodic = False, hermition = False):
                 for k in range(N):
                     new[k] = vec[k]^new[k]
                 Hxy[i,decimal(new)] += - Jxy / 2
-    # add small h to break degeneracy
-    Hh = np.zeros([dim,dim])
-    for i in range(dim):
-        for j in range(N):
+
             Hh[i,i] += h * (basis[i,j] - 1/2)
+
     H = Hz + Hxy + Hh
-    if hermition:
-        H = (H + np.conj(H).T)/2
     return H
 
+#-------------------------------------------------------------------------------
 '''Add constrain: total Sz = S
    For a length = N(N=even) chain, there are N/2 spin-up.
 '''
@@ -133,7 +131,7 @@ def heisenberg(N, Jz, Jxy, h, periodic = False, hermition = False):
    2. Determin two part separately. We only have to figure out the spin
       congigurations of M particles in N/2 sites(M <= N/2)
    '''
-def half_chain(N,M):
+def basis_sz_conserve(N,M):
     '''M spin-up on N sites: M<=N'''
     dim = int(comb(N,M))
     vecs = np.zeros([dim,N],int)
@@ -173,18 +171,48 @@ def half_chain(N,M):
                     vecs[j+1,i] = 1
     return vecs
 
-vec = half_chain(6,4)
-print(vec)
 
 
-def basis_conserve_sz(N,M):
-    '''Build basis of total Sz = M/2
-       M: numer of spin-up, if M <= 1/2'''
-    dim = basis_dim(N,M)
-
-
-    return
-
-def heisenberg_conserve_sz(N):
+def heisenberg_sz_conserve(N,Sz):
     '''Build Hamiltonian with conserved Sz = S'''
+    M = N/2 + Sz
+    if M%1 != 0:
+        print('Error: No such configuration')
+    else:
+        M = int(M)
+    basis = basis_sz_conserve(N,M)
+    dim = len(basis)
+    ''''''
+        if periodic:
+            limit = N
+        else:
+            limit = N-1
+
+        Hz = np.zeros([dim,dim]) # Diagonal: Hz = Jz S^z S^z
+        Hxy = np.zeros([dim,dim]) #Off-diagonal: Hxy = Jxy/2 (S^+ S^-  +  S^- S^+)
+        Hh = np.zeros([dim,dim]) # add small h to break degeneracy
+
+        for i in range(dim):
+            for j in range(limit):
+                next = (j+1) % N
+                Hz[i,i] += - Jz * (basis[i,j] - 1/2) * (basis[i,next] - 1/2)
+
+                vec = np.zeros(N,int)
+                vec[j] = 1
+                vec[next] = 1
+                if basis[i,j]!= basis[i,next]:
+                    new = basis[i,:].copy()
+                    for k in range(N):
+                        new[k] = vec[k]^new[k]
+                    Hxy[i,decimal(new)] += - Jxy / 2
+
+                Hh[i,i] += h * (basis[i,j] - 1/2)
+
+        H = Hz + Hxy + Hh
+
     return
+
+#-------------------------------------------------------------------------------
+'''Program test'''
+H = heisenberg(2, 1, 1, 0, periodic = False)
+print(H)
